3e6409cbd0e405098b59d82d15499b9adb48798f
Eric Freeman <clownhunter@gmail.com>
2015-07-03 20:18:56 -0600

    Added basic camera tracking.

---
+++ src/Assets/Resources/Scripts/Camera/CameraTrack.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Assets.Resources.Scripts.Camera
+{
+    public class CameraTrack : MonoBehaviour
+    {
+        public GameObject GameObject;
+        public Vector3 Offset;
+        public float Leeway;
+
+        void Start()
+        {
+            transform.position = GameObject.transform.position + Offset;
+        }
+
+        void Update()
+        {
+            transform.position = GameObject.transform.position + Offset;
+        }
+    }
+}
