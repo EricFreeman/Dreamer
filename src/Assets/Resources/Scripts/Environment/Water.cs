bcbd74aa55c71d044624e8fc643d260f041f9a6c
Eric Freeman <clownhunter@gmail.com>
2015-07-07 17:54:17 -0600

    Forgot to include these files.

---
+++ src/Assets/Resources/Scripts/Environment/Water.cs
@@ -0,0 +1,52 @@
+﻿using Assets.Resources.Scripts.Extensions;
+using UnityEngine;
+
+namespace Assets.Resources.Scripts.Environment
+{
+    public class Water : MonoBehaviour
+    {
+        public float UpwardForce;
+        public float MaxUpwardForce;
+
+        private bool _isUnderwater;
+        private Rigidbody2D _rigidbody2D;
+
+        private float _startDrag;
+
+        void Start()
+        {
+            _rigidbody2D = GameObject.Find("Player").GetComponent<Rigidbody2D>();
+            _startDrag = _rigidbody2D.drag;
+        }
+
+        void Update()
+        {
+            if (_isUnderwater)
+            {
+                var currentVelocity = _rigidbody2D.velocity;
+                _rigidbody2D.velocity = new Vector2(currentVelocity.x, currentVelocity.y.Clamp(-MaxUpwardForce, MaxUpwardForce));
+                _rigidbody2D.AddForce(new Vector2(0, UpwardForce));
+            }
+        }
+
+        void OnTriggerEnter2D(Collider2D collision)
+        {
+            var col = collision.transform;
+            if (col.name == "Player")
+            {
+                _isUnderwater = true;
+                _rigidbody2D.drag = 1f;
+            }
+        }
+
+        void OnTriggerExit2D(Collider2D collision)
+        {
+            var col = collision.transform;
+            if (col.name == "Player")
+            {
+                _isUnderwater = false;
+                _rigidbody2D.drag = _startDrag;
+            }
+        }
+    }
+}
